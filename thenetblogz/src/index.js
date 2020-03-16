20c15a1691a0f0dd163da7e63a3b2d37e19c5ca7
Polinet <uchpolinet@gmail.com>
2020-03-16 16:06:11 +1100

    wrapped the entire app with Auth0Provider

--- thenetblogz/src/index.js
+++ thenetblogz/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from './contexts/auth0-context';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Auth0Provider>
+        <App />
+    </Auth0Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
