722ccb07da96e101a220993bf93adedc31175d8c
Polinet <uchpolinet@gmail.com>
2020-03-18 11:20:05 +1100

    config domain, client id and redirect uri

--- thenetblogz/src/contexts/auth0-context.js
+++ thenetblogz/src/contexts/auth0-context.js
@@ -8,6 +8,11 @@ export const Auth0Context = createContext();
 // create provider
 export class Auth0Provider extends Component {
     state = { auth0Client: null };
+    config = {
+        domain: process.env.REACT_APP_AUTH0_DOMAIN,
+        client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+        redirect_uri: window.location.origin
+    }
     render() {
         const { message } = this.state;
         const { children } = this.props;
