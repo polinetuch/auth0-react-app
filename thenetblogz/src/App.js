643b1f8199d7a41c9f70497f0a98ef0ec6c0ad17
Polinet <uchpolinet@gmail.com>
2020-03-24 18:23:31 +1100

    if not loading then no user

--- thenetblogz/src/App.js
+++ thenetblogz/src/App.js
@@ -4,15 +4,18 @@ import { Auth0Context } from './contexts/auth0-context';
 
 function App() {
   // useContext so that all Auth0Context is available to use in App.js 
-  const auth0 = useContext(Auth0Context);
+  const { isLoading, user, loginWithRedirect} = useContext(Auth0Context);
   return (
     <div className="App">
       <div className="hero is-info is-fullheight">
         <div className="hero-body">
-          <h1>Login First</h1>
-          <button onClick={auth0.loginWithRedirect} className="button is-danger"> Login</button>
           <div className="container has-text-centered">
-            {auth0.message}
+            {!isLoading && !user && (
+            <>
+              <h1>Click here</h1>
+              <button onClick={loginWithRedirect} className="button is-danger">Login</button>
+            </>
+          )}
           </div>
         </div>
       </div>
