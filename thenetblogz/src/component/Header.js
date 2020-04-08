7b5b965a68a66f75cf523522fd574391e1af7c78
Polinet <uchpolinet@gmail.com>
2020-04-08 14:07:42 +1000

    created a navbar

--- thenetblogz/src/component/Header.js
+++ thenetblogz/src/component/Header.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import { useAuth0 } from './contexts/auth0-context';
+
+export default function Header() {
+    const { isLoading, user, loginWithRedirect, logout } = useAuth0();
+
+    return(
+        <header>
+            <nav className="navbar is-dark">
+                <div className="container">
+                    <div className="navbar-menu is-active">
+                        {/*logo*/}
+                        <div className="navbar-brand">
+                            <button className="navbar-item">
+                                The Net Blogz
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
